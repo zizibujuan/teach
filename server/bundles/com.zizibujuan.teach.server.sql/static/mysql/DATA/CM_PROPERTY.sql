bd04329990f5b4b3b7491527fd0409a55cd40d43
jzw <zhengwei.jin@gmail.com>
2014-05-02 09:33:54 +0800

    删除无用的注释

--- server/bundles/com.zizibujuan.teach.server.sql/static/mysql/DATA/CM_PROPERTY.sql
+++ server/bundles/com.zizibujuan.teach.server.sql/static/mysql/DATA/CM_PROPERTY.sql
@@ -1,4 +1,3 @@
 -- 课程内容git仓库的根目录
 insert into DRIP_PROPERTY_KEY (DBID,PROPERTY_KEY,GROUP_ID,PROPERTY_TYPE) VALUES (101, 'git.repo.root.course', 1, 1);
--- 本地测试目录是/home/jzw/drip_data/
 insert into DRIP_PROPERTY_VALUE_STRING (KEY_ID, PROPERTY_VALUE,I18n_ID) VALUES (101, '/mnt/drip_data/course/','zh_cn');
