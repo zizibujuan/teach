4d6f74bf05643ced6abf5c3a5df8a42be27c4dc0
jzw <zhengwei.jin@gmail.com>
2014-05-08 20:44:12 +0800

    修改表结构，将只能存储学生的表，改为既能存储学生，也能存储老师。

--- server/bundles/com.zizibujuan.teach.server.sql/static/mysql/TABLE/DRIP_CLASS_MEMBER.sql
+++ server/bundles/com.zizibujuan.teach.server.sql/static/mysql/TABLE/DRIP_CLASS_MEMBER.sql
@@ -1,15 +1,16 @@
--- 班级中的学生
+-- 班级中的成员
 -- -----------------------------------------------------
--- Table `DRIP_CLASS_STUDENT` 班级中的学生
+-- Table `DRIP_CLASS_MEMBER` 班级中的成员
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `DRIP_CLASS_STUDENT`;
+DROP TABLE IF EXISTS `DRIP_CLASS_MEMBER`;
 
-CREATE  TABLE IF NOT EXISTS `DRIP_CLASS_STUDENT` (
+CREATE  TABLE IF NOT EXISTS `DRIP_CLASS_MEMBER` (
   `DBID` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键' ,
   `CLASS_ID` BIGINT NOT NULL COMMENT '班级标识',
-  `STUDENT_ID` BIGINT NOT NULL COMMENT '学生标识',
+  `MEMBER_TYPE` CHAR(2) NOT NULL COMMENT '成员类型，01为老师，02为学生',
+  `USER_ID` BIGINT NOT NULL COMMENT '用户标识',
   `CRT_TM` DATETIME NULL COMMENT '创建时间' ,
   `CRT_USER_ID` BIGINT NOT NULL COMMENT '创建人',
   PRIMARY KEY (`DBID`))
 ENGINE = InnoDB
-COMMENT = '班级中的学生';
+COMMENT = '班级中的成员';
