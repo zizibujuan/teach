e65a37d7ce47f147486e1ea4b6fcaf924ca780f5
jzw <zhengwei.jin@gmail.com>
2014-05-14 07:25:22 +0800

    添加系统参数

--- server/bundles/com.zizibujuan.teach.server.sql/static/mysql/DATA/CM_PROPERTY.sql
+++ server/bundles/com.zizibujuan.teach.server.sql/static/mysql/DATA/CM_PROPERTY.sql
@@ -1,3 +1,7 @@
 -- 课程内容git仓库的根目录
 insert into DRIP_PROPERTY_KEY (DBID,PROPERTY_KEY,GROUP_ID,PROPERTY_TYPE) VALUES (101, 'git.repo.root.course', 1, 1);
 insert into DRIP_PROPERTY_VALUE_STRING (KEY_ID, PROPERTY_VALUE,I18n_ID) VALUES (101, '/mnt/drip_data/course/','zh_cn');
+
+-- 即将开始的课程的提前通知时间
+insert into DRIP_PROPERTY_KEY (DBID,PROPERTY_KEY,GROUP_ID,PROPERTY_TYPE) VALUES (102, 'alert.minite.before.lesson', 1, 2);
+insert into DRIP_PROPERTY_VALUE_NUMBER (KEY_ID, PROPERTY_VALUE) VALUES (102, 15);
