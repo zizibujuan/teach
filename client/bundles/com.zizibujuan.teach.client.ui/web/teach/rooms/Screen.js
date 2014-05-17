aaf2d857335ea6d4bf7c890b4043c6fea70e4d09
jzw <zhengwei.jin@gmail.com>
2014-05-17 19:38:31 +0800

    新建教室room客户端脚本

---
+++ client/bundles/com.zizibujuan.teach.client.ui/web/teach/rooms/Screen.js
@@ -0,0 +1,14 @@
+define(["dojo/_base/declare",
+        "dijit/_WidgetBase"], function(
+        		declare,
+        		_WidgetBase){
+	
+	return declare("teach.rooms.Screen", [_WidgetBase], {
+		
+		postCreate: function(){
+			this.inherited(arguments);
+		}
+	
+	});
+	
+});
